a066e508542ecc5b1ef26a94c96afbc5a4e24718
benney <benneykwag@gmail.com>
2014-11-12 09:02:52 +0900

    11월12일 수

--- app/view/chapter3/HBoxLayout.js
+++ app/view/chapter3/HBoxLayout.js
@@ -16,13 +16,13 @@ Ext.define('ext5.view.chapter3.HBoxLayout', {
     }, {
         xtype : 'panel',
         title : '두번째 패널',
-        html : '가로길이는 가변적이다. <br>세로 100px',
+        html : '너비는 가변적이다. <br>높이 100px',
         height: 100,
         flex : 1
     }, {
         xtype : 'panel',
         title : '세번째 패널',
-        html : '가로 150px <br>세로 100px',
+        html : '너비 100px <br>높이 150px',
         height: 150,
         width : 100
     } ]
